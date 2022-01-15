037fb2ad7a92b9a8e47a884f7df105032e92be5c
Honza Pobořil <honza@poboril.cz>
2022-01-15 14:18:32 +0100

    Added link to faq.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex place-content-between items-center absolute top-0 right-0 left-0 h-12 z-10">
+  <div class="absolute top-0 left-0 right-0 z-10 flex items-center h-12 place-content-between">
     <NuxtLink to="/">
       <h1 class="ml-2">
         <img class="h-6" src="~assets/logo_bb.svg" alt="BeruBitcoin.cz">
@@ -7,20 +7,20 @@
     </NuxtLink>
 
     <label role="navigation" class="relative">
-      <input type="checkbox" class="peer sr-only" aria-controls="menu">
-      <div class="space-y-1 p-4 peer-checked:bg-gray-700">
+      <input type="checkbox" class="sr-only peer" aria-controls="menu">
+      <div class="p-4 space-y-1 peer-checked:bg-gray-700">
         <div class="w-5 h-0.5 bg-white" />
         <div class="w-5 h-0.5 bg-white" />
         <div class="w-5 h-0.5 bg-white" />
       </div>
-      <nav id="menu" class="peer-checked:block hidden absolute top-12 right-0 bg-gray-700 w-max">
+      <nav id="menu" class="absolute right-0 hidden bg-gray-700 peer-checked:block top-12 w-max">
         <component
           :is="item.href ? 'a' : 'NuxtLink'"
           v-for="item in menuLinks"
           :key="item.to"
           :to="item.to"
           :href="item.href"
-          class="block py-2 px-4 border-b border-gray-800 hover:bg-gray-800"
+          class="block px-4 py-2 border-b border-gray-800 hover:bg-gray-800"
         >
           {{ item.text }}
         </component>
@@ -36,6 +36,7 @@ export default {
       { to: '/online-podniky', text: 'On-line podniky' },
       { href: 'https://www.berubitcoin.cz/', text: 'O přijímání Bitcoinu' },
       { to: '/pridat-podnik', text: 'Přidat podnik' },
+      { href: 'https://www.berubitcoin.cz/#faq-proc-utracet', text: 'Proč Bitcoin utrácet?' },
       { href: 'https://www.berubitcoin.cz/#kontakt', text: 'Kontakt' }
     ]
   })
