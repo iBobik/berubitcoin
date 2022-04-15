1b57e7d1f28cd04f0ecb72ad56f456619ee9de73
Honza Pobořil <honza@poboril.cz>
2022-04-15 22:18:42 +0200

    Tried to fetch.

--- functions/api/subscribe.js
+++ functions/api/subscribe.js
@@ -1,3 +1,5 @@
-export function onRequestPost (request) {
-  return new Response(JSON.stringify(request.request.body) + JSON.stringify(request.data))
+export function onRequestPost ({ request }) {
+  const data = (await fetch('http://echo.jsontest.com/insert-key-here/insert-value-here/key/value')).json()
+  console.log(data)
+  return new Response(request)
 }
