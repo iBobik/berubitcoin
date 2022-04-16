cf7abec78c3113c517ed6fe9ed5f573e4593db0b
Honza Pobořil <honza@poboril.cz>
2022-04-16 15:09:10 +0200

    Improved styles for subscribe.

--- pages/subscribe.vue
+++ pages/subscribe.vue
@@ -1,20 +1,18 @@
 <template>
-  <div>
+  <div class="container">
     <NavBar />
-    <div class="container">
-      <h1 class="text-lg">Zasílat novinky</h1>
-      <form v-if="!send" @submit.prevent="submit">
-        <label>
-          Tvůj e-mail
-          <input type="email" name="email" required>
-        </label>
-        <input type="hidden" name="topics[]" value="Nová ověřená místa">
-        <input type="submit" value="Přihlásit se">
-      </form>
-      <p v-else>
-        Přihlášeno
-      </p>
-    </div>
+    <h1 class="my-4 text-2xl">Zasílat nová místa</h1>
+    <form v-if="!send" @submit.prevent="submit">
+      <label>
+        Tvůj e-mail
+        <input type="email" name="email" required>
+      </label>
+      <input type="hidden" name="topics[]" value="Nová ověřená místa">
+      <input type="submit" value="Přihlásit se">
+    </form>
+    <p v-else>
+      Přihlášeno
+    </p>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors: {
         gray: colors.neutral, // renamed to 'neutral' in v3.0
-        mapboxBase: '#343332'
+        mapboxBase: '#343332',
+        btcOrange: '#f7931a'
+      },
+      container: {
+        center: true
       },
       scale: {
         200: '2'
