074593078935d4443af33c8f189029fb90d29d59
Honza Pobořil <bobik@ibobik.cz>
2023-07-20 20:37:00 +0200

    Cleaned files after removed features.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,6 @@ export default defineNuxtConfig({
       mapBounds: [[12, 47.7], [23, 51.1]],
       mapStyle: 'dark-v10',
       mapAccessToken: 'pk.eyJ1IjoicG9ib3JpbCIsImEiOiJja3dwY3lxYmMwYmk0MnBwNm96aDIzYWRiIn0.Sz9ldnB1zsjTVRFMdFrz0g',
-      dooFrontendKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmNGI2ZDkyMi1jOTA1LTQ1ODctYmJjZi02ODc4NjE5NWVjYTYiLCJ1bmlxdWVfbmFtZSI6ImFwaVRva2VuSWRfYWYxZmQxNDEtYjJhMi00OTRmLThlZDAtZjU5NzI3MDk2MDZiIiwicHVycG9zZSI6IkFQSVRva2VuIiwiYXBpVG9rZW5JZCI6ImFmMWZkMTQxLWIyYTItNDk0Zi04ZWQwLWY1OTcyNzA5NjA2YiIsIm5iZiI6MTY1Mzc0OTM4MywiZXhwIjo0ODA5NDIyOTgzLCJpYXQiOjE2NTM3NDkzODN9.r59Uf_aGUWSHktkacKB00rAT8D8LjTI9OHqxbYxYNLA'
     },
 
   },
