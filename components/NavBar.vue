7f2a5e5fdf6b811cebcf8645c096cee66af72278
Honza Pobořil <bobik@ibobik.cz>
2024-08-13 17:39:02 +0200

    Fixed menu.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -9,40 +9,50 @@
       </NuxtLink>
     </h1>
 
-    <label role="navigation" class="relative my-0">
-      <input type="checkbox" class="sr-only peer" aria-controls="menu">
-      <div class="p-4 space-y-1 peer-checked:bg-gray-700">
-        <div class="w-5 h-0.5 bg-white" />
-        <div class="w-5 h-0.5 bg-white" />
-        <div class="w-5 h-0.5 bg-white" />
-      </div>
-      <nav id="menu" class="absolute right-0 hidden bg-gray-700 peer-checked:block top-12 w-max">
-        <component
-          :is="item.href ? 'a' : 'NuxtLink'"
-          v-for="item in menuLinks"
-          :key="item.to || item.href"
-          :to="item.to"
-          :href="item.href"
-          class="block px-4 no-underline border-b border-gray-800 hover:bg-gray-800"
-          :class="item.category
-            ? 'pt-4 pb-1 text-xs font-semibold'
-            : 'py-2'
-          "
-        >
-          {{ item.text }}
-        </component>
-      </nav>
-    </label>
+    <Menu as="nav" v-slot="{ open }" class="relative flex-shrink-0 ml-4">
+      <MenuButton class="relative flex p-0 my-0 text-sm" :class="open ? 'bg-gray-700' : 'bg-transparent'">
+        <span class="absolute -inset-1" />
+        <span class="sr-only">Open menu</span>
+        <div class="p-4 space-y-1 peer-checked:bg-gray-700">
+          <div class="w-5 h-0.5 bg-white" />
+          <div class="w-5 h-0.5 bg-white" />
+          <div class="w-5 h-0.5 bg-white" />
+        </div>
+      </MenuButton>
+      <transition enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+        <MenuItems class="absolute right-0 z-10 w-64 max-w-[90vw] py-1 origin-top-right bg-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <MenuItem
+            v-for="item in menuLinks"
+            :key="item.to || item.href"
+            v-slot="{ active }"
+          >
+            <NuxtLink
+              :to="item.to || item.href"
+              :target="item.href ? '_blank' : undefined"
+              class="block px-4 no-underline border-b border-gray-800 hover:bg-gray-800"
+              :class="item.category
+                ? 'pt-4 pb-1 text-xs font-semibold'
+                : 'py-2'
+              "
+            >
+              {{ item.text }}
+            </NuxtLink>
+          </MenuItem>
+        </MenuItems>
+      </transition>
+    </Menu>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+
 const props = defineProps<{
   absolute?: boolean
 }>()
 
 const menuLinks = [
-  { to: '/add-place', text: 'Přidat podnik' },
+  { to: '/add-place', text: 'Přidat nebo upravit podnik' },
   { href: 'https://github.com/ibobik/berubitcoin', text: 'GitHub ↗' },
   { category: true, href: 'https://www.berubitcoin.cz/', text: 'BeruBitcoin.cz' },
   { href: 'https://www.berubitcoin.cz/', text: 'O přijímání Bitcoinu ↗' },
